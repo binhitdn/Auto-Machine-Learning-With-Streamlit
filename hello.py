81b786356ef2b5f8d4d72ca53a2b55f546351f37
Bình Phan <binhpc.21it@vku.udn.vn>
2024-04-16 14:45:31 +0700

    second commit

--- hello.py
+++ hello.py
@@ -1,22 +1,23 @@
 import streamlit as st
 import pandas as pd
 from ydata_profiling import ProfileReport
-import pickle
 from streamlit_option_menu import option_menu
 from streamlit_pandas_profiling import st_profile_report
 import os
-from pycaret.classification import setup, compare_models, pull, save_model
 import warnings
 warnings.filterwarnings("ignore")
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 
 
-st.title("Machine Learnin ")
+
+st.title("Machine Learning")
 st.write("Trước tiên hãy up file lên")
 st.write("Chọn một tùy chọn từ thanh bên trái")
-
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # Khai báo biến df
 df = None
@@ -24,11 +25,7 @@ css_style = {
     "icon": {"color": "black"},
     "icon_selected": {"color": "white"},
     "nav-link": {"--hover-color": "grey"},
-    "nav-link-selected": {"background-color": "#FF4C1B"},
-    "title": {"background-color": "#FF4C1B", "color": "white"},
-    "header": {"background-color": "#FF4C1B"},
-    "header-info": {"background-color": "#FF4C1B"},
-    "header-text": {"color": "white"},
+    "nav-link-selected": {"background-color": "#FF4C1B", "color": "white","font-weight": "400"},
 }
 
 if os.path.exists("sourcedata.csv"):
@@ -48,44 +45,33 @@ def upload_page():
         df.to_csv("sourcedata.csv", index=None)
         st.dataframe(df)
 
-def trang_phan_tich():
+def trang_phan_tich(df):
     st.subheader("Phân Tích Dữ Liệu")
     st.info("Trong phần này, ứng dụng thực hiện một phân tích dữ liệu tự động trên dữ liệu. Điều này giúp người dùng hiểu biết và nắm bắt dữ liệu của mình hơn")
     st.info("GHI CHÚ: Nếu không có dữ liệu được tải lên ở Trang Tải Lên Dữ Liệu, trang này sẽ hiển thị một thông báo lỗi.")
-    bao_cao = ProfileReport(df)
-    st_profile_report(bao_cao)
+    
+    st.write("Thông tin cơ bản về dữ liệu:")
+    st.write(df.info())
 
-
-def trang_hoc():
-    st.subheader("TÍNH TOÁN MÁY HỌC TỰ ĐỘNG")
-    st.info("Trong phần này, ứng dụng xây dựng và huấn luyện các mô hình máy học khác nhau với dữ liệu huấn luyện. Người dùng chỉ cần xác định và nhập biến mục tiêu")
-    st.info("GHI CHÚ: Nếu không có dữ liệu được tải lên ở Trang Tải Lên Dữ Liệu, trang này sẽ hiển thị một thông báo lỗi.")
-    muc_tieu = st.selectbox("Vui lòng chọn đặc trưng mục tiêu của bạn", df.columns)
-    if st.button("Huấn luyện mô hình"):
-        setup(df, target=muc_tieu)
-        setup_df = pull()
-        st.info("Đây là các cài đặt máy học tự động")
-        st.dataframe(setup_df)
-        best_model = compare_models()
-        compare_df = pull()
-        st.info("Đây là hiệu suất của các mô hình máy học")
-        st.dataframe(compare_df)
-        best_model
-
-
-def trang_tai_xuong():
-    st.subheader("DOWNLOAD BEST MODEL")
-    st.info("In this section, the app allows users to download and save their best performing models to their local computers")
-    st.info("NOTE : If no data is uploaded at the Data Upload Page, this page will show an error message.")
-    best_model = compare_models()
-    compare_df = pull()
-    st.info("This is the Performance of the machine learning models")
-    st.dataframe(compare_df)
-    best_model
-    save_model(best_model, "best_model")
-    with open("best_model.pkl", 'rb') as f:
-        st.download_button("Please download best model", f, "auto_trained_model.pkl")
-
+    st.write("Mô tả thống kê cơ bản:")
+    st.write(df.describe())
+    
+    st.write("Mối quan hệ giữa các biến:")
+    sns.pairplot(df)
+    st.pyplot()
+    
+    # Phân tích phân phối của các biến
+    st.write("Phân phối của các biến:")
+    for column in df.columns:
+        plt.figure(figsize=(8, 6))
+        sns.histplot(df[column], kde=True)
+        plt.title(f"Distribution of {column}")
+        plt.xlabel(column)
+        plt.ylabel("Frequency")
+        st.pyplot()
+   # Tạo báo cáo phân tích dữ liệu
+    profile = ProfileReport(df, explorative=True)
+    st_profile_report(profile)
 
 def trang_hoi_quy_tuyen_tinh():
     st.subheader("HỒI QUY TUYẾN TÍNH")
@@ -93,8 +79,7 @@ def trang_hoi_quy_tuyen_tinh():
     st.info("GHI CHÚ: Nếu không có dữ liệu được tải lên ở Trang Tải Lên Dữ Liệu, trang này sẽ hiển thị một thông báo lỗi.")
     
     if df is not None:
-        # Chọn biến độc lập (X) và biến mục tiêu (Y)
-        options = [''] + df.columns.tolist()  # Thêm một lựa chọn trống vào danh sách các biến
+        options = [''] + df.columns.tolist()  
         X = st.selectbox("Chọn biến độc lập (X)", options)
         Y = st.selectbox("Chọn biến mục tiêu (Y)", options)
         
@@ -126,18 +111,44 @@ def trang_hoi_quy_tuyen_tinh():
             plt.ylabel(Y)
             plt.title('Actual vs Predicted')
             plt.legend()
-            st.pyplot(plt)  # Hiển thị biểu đồ trong ứng dụng
+            st.pyplot(plt) 
 
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
+def trang_hoi_quy_logistic():
+    st.subheader("HỒI QUY LOGISTIC")
+    st.info("Trong phần này, ứng dụng thực hiện hồi quy logistic trên dữ liệu được chọn.")
+    st.info("GHI CHÚ: Nếu không có dữ liệu được tải lên ở Trang Tải Lên Dữ Liệu, trang này sẽ hiển thị một thông báo lỗi.")
+    
+    if df is not None:
+        options = [''] + df.columns.tolist()
+        X = st.selectbox("Chọn biến độc lập (X)", options)
+        Y = st.selectbox("Chọn biến mục tiêu (Y)", options)
+    
+        if X != '' and Y != '':
+            st.subheader("Đồ thị hàm sigmoid")
+            x_values = np.linspace(-10, 10, 100)
+            y_values = sigmoid(x_values)
+            fig, ax = plt.subplots(figsize=(10, 6))
+            ax.plot(x_values, y_values, color='blue')
+            ax.axvline(0, color='black', linestyle='--')
+            ax.axhline(0.5, color='red', linestyle='--')
+            ax.set_xlabel('x')
+            ax.set_ylabel('sigmoid(x)')
+            ax.set_title('Sigmoid Function')
+            st.pyplot(fig)
 
 # Thiết lập thanh menu lựa chọn (sidebar)
 with st.sidebar:
     st.image(auto_url, use_column_width=True)
-    st.info("Ứng dụng này cho phép người dùng xây dựng và tải xuống một mô hình học máy tự động bằng cách sử dụng streamlit, pandas profiling và pycaret")
+    st.info("Abc")
     selected = option_menu(
         menu_title=None,
-        options=["Trang Chủ", "Trang Tải Lên Dữ Liệu", "Trang Phân Tích Dữ Liệu", "Trang Máy Học Tự Động", "Trang Tải Xuống Mô Hình",  "Hồi Quy Tuyến Tính"],
-        icons=["home", "cloud-upload", "clipboard-data", "cpu", "download",  "calculator"],
+        options=["Trang Chủ", "Trang Tải Lên Dữ Liệu", "Trang Phân Tích Dữ Liệu", 
+                 
+                 "Hồi Quy Tuyến Tính", "Hồi Quy Logistic"],
+        icons=["home", "cloud-upload", "clipboard-data", "cpu", "download",  "calculator", "chart-line"],
         styles=css_style
    )
     
@@ -148,15 +159,10 @@ elif selected == "Trang Tải Lên Dữ Liệu":
     upload_page()
 
 elif selected == "Trang Phân Tích Dữ Liệu":
-    trang_phan_tich()
+    trang_phan_tich(df)
 
-elif selected == "Trang Máy Học Tự Động":
-    trang_hoc()
-
-elif selected == "Trang Tải Xuống Mô Hình":
-    trang_tai_xuong(df)
-
-elif selected == "Trang Phát Triển":
-    trang_phat_trien()
 elif selected == "Hồi Quy Tuyến Tính":
     trang_hoi_quy_tuyen_tinh()
+
+elif selected == "Hồi Quy Logistic":
+    trang_hoi_quy_logistic()
